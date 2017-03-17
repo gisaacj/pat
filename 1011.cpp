1b6fc1fbdfbacc2e39869fb467c64ef5bf9a0637
806025582 <8060255582@qq.com>
2017-03-17 08:08:59 +0800

    orz

---
+++ 1011.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    double a[10],b[10],c[10];
+	int i,x;
+    scanf("%d",&x);
+    for(i=0;i<x;i++)
+    	scanf("%lf %lf %lf",&a[i],&b[i],&c[i]);
+    	for(i=0;i<x;i++){
+	    	if(a[i]+b[i]>c[i])
+	    		printf("Case #%d: true\n",i+1);
+			else
+				printf("Case #%d: false\n",i+1);
+		}
+    return 0;
+}
